977143253d6ffbb76f63391c800411aa3dd362b0
Romulo Vieira <romulo.vieira777@gmail.com>
2023-08-07 12:00:19 -0300

    Iniciando na Programação com Python (Lógica de Programação Básica)

---
+++ Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/03_Aula_string.py
@@ -0,0 +1,25 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+Dinâmica = Não precisa declarar o tipo de variável
+Forte = Não permite mudar o tipo de variável
+str -> string -> Texto
+Strings são textos que estão dentro de aspas simples ou duplas
+"""
+print(123)
+
+# Aspas simples
+print('Olá Mundo do Python!')
+print(1, 'Olá Mundo do "Python!"')
+
+# Aspas duplas
+print("Olá Mundo do Python!")
+print(2, "Olá Mundo do 'Python!'")
+
+# Escape
+print("Olá Mundo do \"Python!")
+print('Olá Mundo do \'Python!\'')
+
+# r
+print(r"Olá Mundo do \"Python!\"")
