88e8da295391890ddbfad6a7bd91edab88808239
Romulo Vieira <romulo.vieira777@gmail.com>
2023-08-12 13:45:56 -0300

    Iniciando na Programação com Python (Lógica de Programação Básica)

---
+++ Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/08_Aula_Exercicios.py
@@ -0,0 +1,13 @@
+nome = "Felicty"
+sobrenome = "Smoak"
+idade = 35
+ano_nascimento = 1985
+maior_idade = idade >= 18
+altura_metros = 1.75
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("Ano de nascimento:", ano_nascimento)
+print("É maior de idade?", maior_idade)
+print("Altura em metros:", altura_metros)
---
+++ Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/12_Aula_exercicio.py
@@ -0,0 +1,7 @@
+nome = "Felicty Smoak"
+altura = 1.75
+peso = 85
+imc = peso / altura ** 2
+print(f"{nome} tem {altura} de altura,")
+print(f"pesa {peso} quilos e seu IMC é")
+print(f"{imc}")
