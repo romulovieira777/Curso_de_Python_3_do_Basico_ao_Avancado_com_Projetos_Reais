33d9bd030883f0ae3e52be265387946ec25dca48
Romulo Vieira <romulo.vieira777@gmail.com>
2023-09-13 21:04:28 -0300

    Iniciando na Programação com Python (Lógica de Programação Básica)

--- Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/48_Aula_while.py
+++ Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/48_Aula_while.py
@@ -8,9 +8,23 @@ frase = "O Python é uma linguagem de programação "\
 
 
 i = 0
+qtd_apareceu_mais_vezes = 0
+letra_apareceu_mais_vezes = ''
+
 while i < len(frase):
     letra_atual = frase[i]
-    quantas_vezes_letra_apareceu = frase.count(letra_atual)
+    
+    if letra_atual == ' ':
+        i += 1
+        continue
+    
+    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
 
-    print(letra_atual, quantas_vezes_letra_apareceu)
+    if qtd_apareceu_mais_vezes_atual > qtd_apareceu_mais_vezes:
+        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
+        letra_apareceu_mais_vezes = letra_atual
+
     i += 1
+
+print(f'A letra que apareceu mais vezes foi: '
+      f'{letra_apareceu_mais_vezes}, que apareceu, {qtd_apareceu_mais_vezes}, vezes.')
---
+++ Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/50_Aula_range_for.py
@@ -0,0 +1,12 @@
+"""
+For + Range
+range -> (start, stop.step)
+"""
+numeros = range(10)
+numeros = range(5, 10)
+numeros = range(5, 10, 2)
+
+print(numeros)
+
+for numero in numeros:
+    print(numero)
