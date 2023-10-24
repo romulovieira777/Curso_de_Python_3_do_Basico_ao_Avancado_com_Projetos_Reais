322ece7cbdf3ad7013102f29895de2d399ff8059
Romulo Vieira <romulo.vieira777@gmail.com>
2023-10-24 14:42:04 -0300

    Iniciando na Programação com Python (Lógica de Programação Básica)

--- Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/79_Aula_possiveis_problemas_e_solucoes.py
+++ Secao_03_Iniciando_Na_Programacao_Com_Python_Logica_De_Programacao_Basica/79_Aula_possiveis_problemas_e_solucoes.py
@@ -24,22 +24,16 @@ contrário disso:
 
 O segundo dígito do CPF é 0
 """
-import re
-import sys
+import random
 
 
-entrada = input('CPF [746.824.890-70]: ')
-#cpf_enviado_usuario = '746.824.890-70'.replace('.', '').replace(' ', '').replace('-', '')
-cpf_enviado_usuario = re.sub(r'[^0-9]', '', entrada)
-nove_digitos = cpf_enviado_usuario[:9]
+nove_digitos = ''
 
-entrada_e_sequencial = entrada == entrada[0] * len(entrada)
-
-if entrada_e_sequencial:
-    print('Você enviou dados sequenciais.')
-    sys.exit()
+for number in range(9):
+    nove_digitos += str(random.randint(0, 9))
 
 contador_regressivo_1 = 10
+
 resultado_digito_1 = 0
 
 for digito in nove_digitos:
@@ -47,23 +41,19 @@ for digito in nove_digitos:
     contador_regressivo_1 -= 1
 
 digito_1 = (resultado_digito_1 * 10) % 11
-digito_1 = digito_1 if digito_1 < 10 else 0
+digito_1 = digito_1 if digito_1 < 9 else 0
 
 dez_digitos = nove_digitos + str(digito_1)
 contador_regressivo_2 = 11
-
 resultado_digito_2 = 0
 
 for digito in dez_digitos:
     resultado_digito_2 += int(digito) * contador_regressivo_2
-    resultado_digito_2 -= 1
+    contador_regressivo_2 -= 1
 
 digito_2 = (resultado_digito_2 * 10) % 11
-digito_2 = digito_2 if digito_2 < 10 else 0
+digito_2 = digito_2 if digito_2 < 9 else 0
 
-cpf_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
+cof_gerado_pelo_calculo = f'{nove_digitos}{digito_1}{digito_2}'
 
-if cpf_enviado_usuario == cpf_gerado_pelo_calculo:
-    print(f'CPF: {cpf_enviado_usuario} é válido.')
-else:
-    print(f'CPF: {cpf_enviado_usuario} é inválido.')
+print(f'CPF gerado pelo cálculo: {cof_gerado_pelo_calculo}')
